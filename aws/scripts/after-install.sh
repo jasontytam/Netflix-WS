3140ca2893a08439f8f3e9574401f3eae5e6aece
Jason Tam <jasontytam@gmail.com>
2023-01-13 23:16:01 +0000

    remove s3 cp wildcard

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,4 +3,4 @@ set -xe
 
 
 # Copy war file from S3 bucket 
-aws s3 cp s3://tamj-netflix-ws-deploy/Netflix-App-*-SNAPSHOT.jar /usr/local/codedeployresources/
+aws s3 cp s3://tamj-netflix-ws-deploy/Netflix-App-0.0.1-SNAPSHOT.jar /usr/local/codedeployresources/
