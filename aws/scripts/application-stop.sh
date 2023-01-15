e8632d4ca6775b03463c5866b96181ee8c26c1a7
Jason Tam <jasontytam@gmail.com>
2023-01-15 21:23:17 +0000

    fix appspec scripts

--- aws/scripts/application-stop.sh
+++ aws/scripts/application-stop.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 set -x
 
-pkill -9 -f Netflix-App
+
+SLEEP_TIME=3
+
+PID=`ps axf |grep Netflix-App |grep java | grep -o '^[ ]*[0-9]*' |xargs`
+if [ ! -z "$PID" ]; then
+  echo "Process is running (pid: $PID). Issuing kill..."
+  pkill -9 -f Netflix-App
+  sleep $SLEEP_TIME
+  exit 0
+else
+  echo "Process is not running."
+  exit 0
+fi
--- aws/scripts/validate-service.sh
+++ aws/scripts/validate-service.sh
@@ -6,9 +6,10 @@ SLEEP_TIME=3
 
 for i in `seq 1 $NUMBER_OF_ATTEMPTS`;
 do
-  ps |grep Netflix-App |grep java | grep -o '^[ ]*[0-9]*' > /dev/null
-  if [ $? -eq 0 ]; then
-    echo "Process is running."
+  PID=`ps axf |grep Netflix-App |grep java | grep -o '^[ ]*[0-9]*' |xargs`
+  if [ ! -z "$PID" ]; then
+    echo "Process is running (pid: $PID)."
+    exit 0
   else
     echo "Process is not running. retrying..."
   fi
