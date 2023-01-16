13541003d44dfa917f698b853e54bb64f2f54508
Jason Tam <jasontytam@gmail.com>
2023-01-16 16:43:53 +0000

    mv console logging to file

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -xe
 
-java -jar /usr/local/Netflix-WS/Netflix-App-0.0.1-SNAPSHOT.jar &
+java -jar /usr/local/Netflix-WS/Netflix-App-0.0.1-SNAPSHOT.jar 2>&1 > Netflix-App.console.$(date '+%Y%m%d_%H%M%S').log &
