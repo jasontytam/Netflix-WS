49bf9b56e1d7070c6554dfc7f7fe4967f9a1a6e7
Jason Tam <jasontytam@gmail.com>
2023-01-16 16:55:52 +0000

    fix start script

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -xe
 
-java -jar /usr/local/Netflix-WS/Netflix-App-0.0.1-SNAPSHOT.jar 2>&1 > Netflix-App.console.$(date '+%Y%m%d_%H%M%S').log &
+cd /usr/local/Netflix-WS/
+java -jar Netflix-App-0.0.1-SNAPSHOT.jar 2>&1 > ./log/Netflix-App.console.$(date '+%Y%m%d_%H%M%S').log &
